630cc5eed6a99951ebdcce3f27e01e41c110b21b
Krishna Kumar Singh <03kksingh@gmail.com>
2022-07-29 23:21:34 +0530

    Map() rendered subscribers dynamically

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    let subscribers =[ 
+      { id:1,
+        name: "krishna singh",
+        phone: "3333333333"  
+      },
+      { id:1,
+        name: "manoj kumar",
+        phone: "1010101010"  
+      }
+    ];
+
     return (
       <div>
 
@@ -15,6 +27,15 @@ class App extends Component {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
+          {
+            subscribers.map(sub =>{
+              return <div key ={sub.id} className="grid-container">
+              <span className='grid-item'>{sub.name}</span>
+              <span className='grid-item'>{sub.phone}</span>
+              </div>
+            })
+          }
         </div>
       </div>
     );
