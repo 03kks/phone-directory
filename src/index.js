69216ca8c64c1ee8df7b8b53670614d9241aec9a
Krishna Kumar Singh <03kksingh@gmail.com>
2022-08-04 16:16:18 +0530

    delete component added

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import PhoneDirectory from './PhoneDirectory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <PhoneDirectory />
-  //</React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
