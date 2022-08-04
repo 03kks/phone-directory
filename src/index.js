56543fabe11335403d7bdb99c8bc8f23a3f30efa
Krishna Kumar Singh <03kksingh@gmail.com>
2022-08-04 16:09:10 +0530

    Routing done in phone directory

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 import PhoneDirectory from './PhoneDirectory';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <PhoneDirectory />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
